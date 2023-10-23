424fb5f48f1a3d0de98da365b691e6668a49bf21
zummon <zummon.space@gmail.com>
2023-10-23 17:12:19 +0700

    save

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}","./docs/**/*.{html,js}"],
+  content: ["./docs/**/*.{html,js}"],
   theme: {
     extend: {},
   },
